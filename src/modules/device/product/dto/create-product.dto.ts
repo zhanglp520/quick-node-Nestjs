0dc63d5e479be1f355d5b6d317189d665dfba266
土豆哥 <zhanglp15229380174@163.com>
2023-08-30 15:05:59 +0800

    perf: 切换log数据库

--- src/modules/device/product/dto/create-product.dto.ts
+++ src/modules/device/product/dto/create-product.dto.ts
@@ -1,7 +1,20 @@
 export class CreateProductDto {
   productId: string;
   productName: string;
+  categoryMode: number;
   productType: number;
   deviceType: number;
+  accessProtocol: number;
+  dataProtocol: number;
+  networkMode: number;
+  accessKey: number;
+  productKey: number;
+  productSecret: number;
+  enabled: boolean;
+  published: boolean;
+  createTime: Date;
+  updateTime: Date;
+  publishTime: Date;
+  unpublishTime: Date;
   remark: string;
 }
--- src/modules/device/product/entities/product.entity.ts
+++ src/modules/device/product/entities/product.entity.ts
@@ -1,5 +1,6 @@
 import { Transform } from "class-transformer";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const moment = require("moment");
 
 @Entity("dev_products")
@@ -10,17 +11,40 @@ export class ProductEntity extends BaseEntity {
   productId: string;
   @Column({ type: "varchar", name: "product_name" })
   productName: string;
+  @Column({ type: "int", name: "category_mode" })
+  categoryMode: number;
   @Column({ type: "int", name: "product_type" })
   productType: number;
   @Column({ type: "int", name: "device_type" })
   deviceType: number;
+  @Column({ type: "int", name: "access_protocol" })
+  accessProtocol: number;
+  @Column({ type: "int", name: "data_protocol" })
+  dataProtocol: number;
+  @Column({ type: "int", name: "network_mode" })
+  networkMode: number;
+  @Column({ type: "int", name: "access_key" })
+  accessKey: number;
+  @Column({ type: "int", name: "product_key" })
+  productKey: number;
+  @Column({ type: "int", name: "product_secret" })
+  productSecret: number;
   @Column({ type: "int", default: 0 })
   enabled: boolean;
-  @Transform((createTime: any) =>
-    moment(createTime.value).format("YYYY-MM-DD HH:mm:ss")
-  )
+  @Column({ type: "int", default: 0 })
+  published: boolean;
+  @Transform((time: any) => moment(time.value).format("YYYY-MM-DD HH:mm:ss"))
   @Column({ type: "datetime", name: "create_time", default: new Date() })
   createTime: Date;
+  @Transform((time: any) => moment(time.value).format("YYYY-MM-DD HH:mm:ss"))
+  @Column({ type: "datetime", name: "update_time", default: new Date() })
+  updateTime: Date;
+  @Transform((time: any) => moment(time.value).format("YYYY-MM-DD HH:mm:ss"))
+  @Column({ type: "datetime", name: "publish_time", default: new Date() })
+  publishTime: Date;
+  @Transform((time: any) => moment(time.value).format("YYYY-MM-DD HH:mm:ss"))
+  @Column({ type: "datetime", name: "unpublish_time", default: new Date() })
+  unpublishTime: Date;
   @Column({ type: "varchar" })
   remark: string;
 }
